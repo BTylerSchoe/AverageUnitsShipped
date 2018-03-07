1e2a499a639c73cace00d7aafdfcb7a4633110b8
Owner <Owner@BRAYDEN-SCHOENA>
2018-03-07 02:41:05 -0500

    Add project files.

---
+++ Lab1BraydenSchoenauUnitsShipped/AverageUnitsShipped.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLab1BraydenSchoenauUnitsShipped
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtValueDisplay = New System.Windows.Forms.TextBox()
+        Me.txtUserValue = New System.Windows.Forms.TextBox()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.lblUnitsPrompt = New System.Windows.Forms.Label()
+        Me.lblDay = New System.Windows.Forms.Label()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblCalculatedAverage = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtValueDisplay
+        '
+        Me.txtValueDisplay.Location = New System.Drawing.Point(21, 57)
+        Me.txtValueDisplay.Multiline = True
+        Me.txtValueDisplay.Name = "txtValueDisplay"
+        Me.txtValueDisplay.ReadOnly = True
+        Me.txtValueDisplay.Size = New System.Drawing.Size(369, 206)
+        Me.txtValueDisplay.TabIndex = 3
+        Me.txtValueDisplay.TabStop = False
+        '
+        'txtUserValue
+        '
+        Me.txtUserValue.Location = New System.Drawing.Point(146, 29)
+        Me.txtUserValue.Name = "txtUserValue"
+        Me.txtUserValue.Size = New System.Drawing.Size(100, 22)
+        Me.txtUserValue.TabIndex = 1
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(21, 343)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(119, 35)
+        Me.btnEnter.TabIndex = 5
+        Me.btnEnter.Text = "Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'lblUnitsPrompt
+        '
+        Me.lblUnitsPrompt.AutoSize = True
+        Me.lblUnitsPrompt.Location = New System.Drawing.Point(18, 34)
+        Me.lblUnitsPrompt.Name = "lblUnitsPrompt"
+        Me.lblUnitsPrompt.Size = New System.Drawing.Size(44, 17)
+        Me.lblUnitsPrompt.TabIndex = 0
+        Me.lblUnitsPrompt.Text = "Units:"
+        '
+        'lblDay
+        '
+        Me.lblDay.AutoSize = True
+        Me.lblDay.Location = New System.Drawing.Point(345, 29)
+        Me.lblDay.Name = "lblDay"
+        Me.lblDay.Size = New System.Drawing.Size(45, 17)
+        Me.lblDay.TabIndex = 2
+        Me.lblDay.Text = "Day 1"
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(146, 343)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(119, 35)
+        Me.btnReset.TabIndex = 6
+        Me.btnReset.Text = "&Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(271, 343)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(119, 35)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "&Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblCalculatedAverage
+        '
+        Me.lblCalculatedAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblCalculatedAverage.Location = New System.Drawing.Point(21, 282)
+        Me.lblCalculatedAverage.Name = "lblCalculatedAverage"
+        Me.lblCalculatedAverage.Size = New System.Drawing.Size(369, 47)
+        Me.lblCalculatedAverage.TabIndex = 4
+        '
+        'frmLab1BraydenSchoenauUnitsShipped
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(414, 390)
+        Me.Controls.Add(Me.lblCalculatedAverage)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.lblDay)
+        Me.Controls.Add(Me.lblUnitsPrompt)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.txtUserValue)
+        Me.Controls.Add(Me.txtValueDisplay)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Name = "frmLab1BraydenSchoenauUnitsShipped"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average Units Shipped"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtValueDisplay As TextBox
+    Friend WithEvents txtUserValue As TextBox
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents lblUnitsPrompt As Label
+    Friend WithEvents lblDay As Label
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblCalculatedAverage As Label
+End Class
---
+++ Lab1BraydenSchoenauUnitsShipped/AverageUnitsShipped.vb
@@ -0,0 +1,157 @@
+﻿''' <summary>
+''' Author: Brayden Schoenau
+''' Project Name: Lab1AverageUnitsShipped
+''' Date: 02-Feb-2018
+''' Description: A windows form application that accepts user input for the daily sales in a week
+'''              And displays them to the user as well as the calculated average weekly sales.
+''' </summary>
+''' 
+Public Class frmLab1BraydenSchoenauUnitsShipped
+
+    'Initalize variable userInput As an Integer & set it equal to 0
+    Dim userInput As Integer = 0
+    'Initalize variable sumOfSales As a Double & set it equal to 0.0
+    Dim sumOfSales As Double = 0.0
+    'Initalize variable weeklyAverageSales As a Double & set it equal to 0.0
+    Dim weeklyAverageSales As Double = 0.0
+    'Initialize array currentDay and set its index value to 6
+    Dim currentDay(6) As Integer
+    'Initialize constant daySeven as a double and set it equal to 7.0
+    Const DaySeven As Double = 7.0
+    'Initalize variable indexCounter As an Integer & set it equal to 0
+    Dim indexCounter As Integer = 0
+    'Initalize variable dayCounter As an Integer & set it equal to 2
+    Dim dayCounter As Integer = 2
+
+
+
+    ''' <summary>
+    ''' Enter a number and click Enter. 
+    ''' It will test If the number is an Integer 
+    '''    1. store it in array currentDay 
+    '''    2. add input to sumOfSales variable
+    '''    3. Display input in txtValueDisplay textbox
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' 
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+
+        ' If the value of txtUservalue is a valid integer store it in userInput variable
+        If (Integer.TryParse(txtUserValue.Text, userInput)) Then
+            ' If user Input is greater than or equal to 0 and less or equal to 1000
+            If (userInput >= 0 And userInput <= 1000) Then
+
+                ' currentDay array current index is equal to userInput
+                currentDay(indexCounter) = userInput
+                'txtValueDisplay text is equal to userInput, add new line
+                txtValueDisplay.Text &= userInput.ToString & vbCrLf
+                'lblDay text is equal to "Day " & dayCounter
+                lblDay.Text = "Day " & dayCounter
+                'dayCounter is equal to dayCounter + 1
+                dayCounter += 1
+                'indexCounter is equal to indexCounter + 1
+                indexCounter += 1
+                'txtUserValue text is equal to an empty string
+                txtUserValue.Text = ""
+                ' Move focus to txtUserValue text box
+                txtUserValue.Select()
+
+                'If indexCounter variable is greater than or equal to daySeven variable
+                If (indexCounter >= DaySeven) Then
+
+                    'for loop to calculate sum of sales
+                    For i As Integer = 0 To currentDay.Length - 1
+                        'sumOfSales vairable is equal to sumOfSAles + the value in the currentDay array at index i
+                        sumOfSales += currentDay(i)
+                    Next
+
+                    'lblDay text is equal to "Day 7"
+                    lblDay.Text = "Day 7"
+                    'txtUserValue set to read only
+                    txtUserValue.ReadOnly = True
+                    'weeklyAverageSales is equal to the the sum of sales divided by 7 and rounded to 2 decimal places
+                    weeklyAverageSales = Math.Round((sumOfSales / DaySeven), 2)
+                    'lblCalculatedAverage text is equal to "Average per day: " & weeklyAverageSales converted to a string
+                    lblCalculatedAverage.Text = "Average per day: " & weeklyAverageSales.ToString
+                    'Set focus to btnReset control
+                    btnReset.Select()
+                    'Disable btnEnter button
+                    btnEnter.Enabled = False
+
+                End If
+
+            Else
+                'If userInput is greater than 1000 or less than 0 Display message 
+                ' "Please ensure input is between 0 and 1000 inclusive"
+                MessageBox.Show("Please ensure input is between 0 and 1000 inclusive")
+                'txtUserValue text is equal to an empty string
+                txtUserValue.Text = ""
+                'Set focus to txtUserValue text box
+                txtUserValue.Select()
+
+            End If
+
+        Else
+            'If userInput is not a valid integer display message "Please ensure input is numeric"
+            MessageBox.Show("Please ensure input is numeric")
+            'txtUserValue text is equal to an empty string
+            txtUserValue.Text = ""
+            'Set focus to txtUserValue text box
+            txtUserValue.Select()
+
+        End If
+
+    End Sub
+
+
+    ''' <summary>
+    ''' Exit button event handler exits the form application
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        'Close the current from application
+        Me.Close()
+    End Sub
+
+
+    ''' <summary>
+    ''' Reset button event handler resets the from application
+    '''  when the user clicks reset button
+    ''' Set all numeric values to 0
+    ''' Set all textboxes to empty 
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+
+        'Set value of userInput to 0
+        userInput = 0
+        'Set value of sumOfSales to 0
+        sumOfSales = 0
+        'Set value of weeklyAAverageSales to 0
+        weeklyAverageSales = 0
+        'Set value of indexCounter to 0
+        indexCounter = 0
+        'Set value of dayCounter to 0
+        dayCounter = 1
+        'Set value of txtUserValue text to an empty string
+        txtUserValue.Text = ""
+        'Set value of txtValueDisplay text to an empty string
+        txtValueDisplay.Text = ""
+        'Set lblDay text to "Day " & dayCounter value
+        lblDay.Text = "Day " & dayCounter
+        'Set value of lblCalculatedAverage text to an empty string
+        lblCalculatedAverage.Text = ""
+        'Set txtUserValue text box read only property to false
+        txtUserValue.ReadOnly = False
+        'Set focus to txtUserValue text box
+        txtUserValue.Select()
+        'Enable btnEnter control
+        btnEnter.Enabled = True
+    End Sub
+
+End Class
